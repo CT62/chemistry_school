11a4594c26175760d844f061dad3cf0c8cbb509d
clive t <clivethompson@gmail.com>
2023-10-10 22:00:17 +0100

    commit

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -5,8 +5,8 @@ export default function CallToAction(){
   <div className="relative py-16">
     <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
     <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 ">
-            <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
-            <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+            <div className="blur-[106px] h-56 bg-gradient-to-br to-purple-400 from-blue-700"></div>
+            <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400  to-indigo-600"></div>
         </div>
       <div className="relative">
         <div className="mt-6 m-auto space-y-6 md:w-8/12 lg:w-7/12">
