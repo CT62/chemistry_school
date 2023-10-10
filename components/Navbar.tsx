6fcd4a6501f57219085c0b7f8d6e9bf425acaa87
clive t <clivethompson@gmail.com>
2023-10-10 20:53:54 +0100

    commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import './global.css'
+export default function Header(){
+    return(
+    <header>
+        <nav className="z-10 w-full absolute bg-opacity-30">
+        <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+                <div className="flex flex-wrap items-center justify-between py-2 gap-6 md:py-4 md:gap-0 relative">
+                    <input aria-hidden="true" type="checkbox" name="toggle_nav" id="toggle_nav" className="hidden peer" />
+                    <div className="relative z-20 w-full flex justify-between lg:w-max md:px-0">
+                        <a href="#home" aria-label="logo" className="flex space-x-2 items-center">
+                            <span className="text-2xl font-bold text-white">C.S.G</span>
+                        </a>
+                        
+                        <div className="relative flex items-center lg:hidden max-h-10">
+                            <label role="button" htmlFor="toggle_nav" aria-label="humburger" id="hamburger" className="relative  p-6 -mr-6">
+                                <div aria-hidden="true" id="line" className="m-auto h-0.5 w-5 rounded bg-sky-900 dark:bg-gray-300 transition duration-300"></div>
+                                <div aria-hidden="true" id="line2" className="m-auto mt-2 h-0.5 w-5 rounded bg-sky-900 dark:bg-gray-300 transition duration-300"></div>
+                            </label>
+                        </div>
+                    </div>
+                    <div aria-hidden="true" className="fixed z-10 inset-0 h-screen w-screen bg-white backdrop-blur-2xl origin-bottom scale-y-0 transition duration-500 peer-checked:origin-top peer-checked:scale-y-100 lg:hidden bg-white"></div>
+                    <div className="flex-col z-20 flex-wrap gap-6 p-8 rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 justify-end w-full invisible opacity-0 translate-y-1  absolute top-full left-0 transition-all duration-300 scale-95 origin-top 
+                                lg:relative lg:scale-100 lg:peer-checked:translate-y-0 lg:translate-y-0 lg:flex lg:flex-row lg:items-center lg:gap-0 lg:p-0 lg:bg-transparent lg:w-7/12 lg:visible lg:opacity-100 lg:border-none
+                                peer-checked:scale-100 peer-checked:opacity-100 peer-checked:visible lg:shadow-none 
+                                bg-cyan-500 bg-opacity-30 border-white">
+                    
+                        <div className="text-white lg:pr-4 lg:w-auto w-full lg:pt-0 font-extrabold">
+                            <ul className="tracking-wide font-medium lg:text-sm flex-col flex lg:flex-row gap-6 lg:gap-0">
+                                <li>
+                                    <a href="courses" className="block md:px-4 transition hover:text-primary">
+                                        <span>Courses</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#blog" className="block md:px-4 transition hover:text-primary">
+                                        <span>Cart</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    )
+}
---
+++ components/Profile.tsx
@@ -0,0 +1,54 @@
+'use client'
+import {motion} from "framer-motion"
+export default function Profile(){
+	return(
+	<>
+		<motion.div
+		initial={{ opacity: 0, scale: 0.5 }}
+   		 animate={{ opacity: 1, scale: 1 }}
+    		transition={{ duration: 1 }}
+  		>
+		<div className="px-5 flex justify-center">
+		<div className="border-1-4 lg:w-1/2 w-auto p-4 bg-gray-800 rounded border-2 border-gray-700">
+		<div className="flex justify-center">
+			<div className="h-32 w-32 m-0">
+				<img src="/Profile.jpg" className="rounded-full" />
+			</div>
+		</div>
+		<div className="py-5 flex justify-center">
+			<div className="text-white font-bold text-center text-2xl">
+				Hello! I'm Lena Kadri, the proud owner of Chemistry School of Growth.
+			</div>
+		</div>
+		<div className="py-2 flex justify-center">
+			<div className="text-gray-300 font-semibold text-center">
+			With a Ph.D. earned at Trinity College and three years of teaching experience, I've cultivated a passion for guiding students through the fascinating world of senior cycle chemistry.
+			</div>
+		</div>
+		<div className="flex justify-center items-center gap-6 my-6">
+                        <a className="text-gray-700 hover:text-cyan-500" aria-label="Visit My LinkedIn" href="https://ie.linkedin.com/in/lena-kadri-7b4ab3158"
+                            target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="h-6">
+                                <path fill="currentColor"
+                                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z">
+                                </path>
+                            </svg>
+                        </a>
+                        <a className="text-gray-700 hover:text-cyan-500" aria-label="Visit My Facebook" href="https://www.facebook.com/chemistryschoolofgrinds/"
+                            target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="h-6">
+                                <path fill="currentColor"
+                                    d="m279.14 288 14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
+                                </path>
+                            </svg>
+                        </a>
+                    </div>
+		</div>	
+		</div>
+		</motion.div>
+
+	</>
+
+)}
+
+
+
+
