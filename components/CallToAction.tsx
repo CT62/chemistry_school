2e28e49eea373abf5afe49a0003298523ee3bf1f
clive t <clivethompson@gmail.com>
2023-10-10 22:36:22 +0100

    commit

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import {TypeAnimation} from 'react-type-animation'
 export default function CallToAction(){
   return(
@@ -38,10 +39,10 @@ export default function CallToAction(){
               </a>
               <a
                 href="about"
-                className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
+                className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 before:border-gray-700 :before:bg-gray-800 sm:w-max"
               >
                 <span
-                  className="relative text-base font-semibold text-primary dark:text-white"
+                  className="relative text-base font-semibold text-primary text-white"
                   >Learn more</span>
             </a>
           </div>
@@ -51,3 +52,4 @@ export default function CallToAction(){
   </div>
   )
 }
+
