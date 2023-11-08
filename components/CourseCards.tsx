056f26ec50411d3d32b6a3b00bb41fa4849b2a35
clive T <clivethompson09@gmail.com>
2023-11-08 22:33:58 +0000

    commit

--- components/CourseCards.tsx
+++ components/CourseCards.tsx
@@ -17,7 +17,7 @@ interface Props {
   review: string;
 }
 
-export function CourseCard({ fakeprice,title, points, time, price, titledesc, reviews,reviewscount,}:Props){
+export function CourseCard({ fakeprice,title, points, time, price, titledesc, reviews,reviewsCount}:Props){
   const [isAddedToCart, setIsAddedToCart] = useState(false);
   const {showReviews,setReviews} = useMyContext();
   const noPoints=points[0]=="" ? true: false;
@@ -65,7 +65,7 @@ export function CourseCard({ fakeprice,title, points, time, price, titledesc, re
 </svg><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="mr-0.5 mt-[0.25rem]" viewBox="0 0 16 16">
   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
 </svg>
-  <div className="text-black pl-1">(</div><button onClick={toggleReviews} className="text-black underline underline-offset-1">{reviewscount} reviews</button><div className="text-black">)</div>
+  <div className="text-black pl-1">(</div><button onClick={toggleReviews} className="text-black underline underline-offset-1">{reviewsCount} reviews</button><div className="text-black">)</div>
   </div> 
 	</div>
   {showReviews && (
